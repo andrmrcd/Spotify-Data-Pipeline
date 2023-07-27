975127658b97539168b5783888285f2d1bf33d07
dbop01 <76557984+andrmrcd@users.noreply.github.com>
2023-07-27 10:38:38 +0800

    Update daily_dag.py

--- dags/daily_dag.py
+++ dags/daily_dag.py
@@ -4,7 +4,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
 # Import the main function from Spotify_ETL module
-from modules.Spotify_ETL import main
+from modules.Spotify_ETL import main as run_etl
 
 # Define default arguments for the DAG
 default_args= {
@@ -23,12 +23,12 @@ dag = DAG(
     'spotify_dag',
     default_args=default_args,
     description='Spotify_ETL DAG',
-    schedule_interval='0 2 * * *',
-)   
+    schedule_interval='0 2 * * *', # Set to 10AM PH Time
+)  
 
 task1 = PythonOperator(
     task_id='SPOTIFY_ETL',
-    python_callable=main,
+    python_callable=run_etl,
     dag=dag
 )
 
